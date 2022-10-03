83b3ec9fc4f7767491b4330ee745985b90e51fc0
fathergasc <quaglioniandrea@gmail.com>
2022-10-03 14:18:02 +0200

    created Comic model, relative controller&migration

---
+++ database/migrations/2022_10_03_120729_create_comics_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title', 100);
+            $table->text('description')->nullable();
+            $table->string('thumb', 255);
+            $table->decimal('price', $precision = 6, $scale = 2);
+            $table->string('series', 100);
+            $table->date('sale_date');
+            $table->string('type', 100);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
