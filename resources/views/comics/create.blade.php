e0bf9542b63115371bac319843d54d7ea8a21b02
fathergasc <quaglioniandrea@gmail.com>
2022-10-05 14:55:53 +0200

    added error msgs under each input of create form

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <div class="container">
     @if ($errors->any())
-        <div class="alert alert-danger">
+        <div class="alert alert-warning">
             <ul>
                 @foreach ($errors->all() as $error)
                     <li>{{ $error }}</li>
@@ -21,40 +21,62 @@
 
         <div class="mb-3">
             <label for="title" class="form-label">Title</label>
-            <input type="text" class="form-control" id="title" name="title" value="{{old('title')}}">
+            <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{old('title')}}">
+
+            @error('title')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="mb-3">
             <label for="description" class="form-label">Description</label>
-            <textarea name="description" id="description" cols="30" rows="10" class="form-control">{{old('description')}}</textarea>
+            <textarea name="description" id="description" cols="30" rows="10" class="form-control @error('description') is-invalid @enderror">{{old('description')}}</textarea>
+            @error('description')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="mb-3">
             <label for="thumb" class="form-label">Thumb</label>
-            <input type="text" class="form-control" id="thumb" name="thumb" value="{{old('thumb')}}">
+            <input type="text" class="form-control @error('thumb') is-invalid @enderror" id="thumb" name="thumb" value="{{old('thumb')}}">
+            @error('thumb')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="mb-3">
             <label for="price" class="form-label">Price</label>
-            <input type="text" class="form-control" id="price" name="price" value="{{old('price')}}">
+            <input type="text" class="form-control @error('price') is-invalid @enderror" id="price" name="price" value="{{old('price')}}">
+            @error('price')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="mb-3">
             <label for="series" class="form-label">Series</label>
-            <input type="text" class="form-control" id="series" name="series" value="{{old('series')}}">
+            <input type="text" class="form-control @error('series') is-invalid @enderror" id="series" name="series" value="{{old('series')}}">
+            @error('series')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="mb-3">
             <label for="sale_date" class="form-label">Sale Date</label>
-            <input type="date" class="form-control" id="sake_date" name="sale_date" value="{{old('sale_date')}}">
+            <input type="date" class="form-control @error('sale_date') is-invalid @enderror" id="sake_date" name="sale_date" value="{{old('sale_date')}}">
+            @error('sale_date')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
         </div>
 
         <div class="mb-3">
             <label for="type" class="form-label">Type</label>
-            <select class="form-select" id="type" name="type">
+            <select class="form-select @error('type') is-invalid @enderror" id="type" name="type">
                 <option {{(old('type') == 'comic book')? 'selected':''}} value="comic book">Comic Book</option>
                 <option {{(old('type') == 'graphic novel')? 'selected':''}} value="graphic novel">Graphic Novel</option>
             </select>
+            @error('type')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
         </div>
 
         <button type="submit" class="btn btn-primary">Submit</button>
