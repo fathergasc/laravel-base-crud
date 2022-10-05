d0ddc295d09a29d9a153d7e34e13df84f770a260
fathergasc <quaglioniandrea@gmail.com>
2022-10-05 14:46:59 +0200

    added old() to create form values

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -21,39 +21,39 @@
 
         <div class="mb-3">
             <label for="title" class="form-label">Title</label>
-            <input type="text" class="form-control" id="title" name="title">
+            <input type="text" class="form-control" id="title" name="title" value="{{old('title')}}">
         </div>
 
         <div class="mb-3">
             <label for="description" class="form-label">Description</label>
-            <textarea name="description" id="description" cols="30" rows="10" class="form-control"></textarea>
+            <textarea name="description" id="description" cols="30" rows="10" class="form-control">{{old('description')}}</textarea>
         </div>
 
         <div class="mb-3">
             <label for="thumb" class="form-label">Thumb</label>
-            <input type="text" class="form-control" id="thumb" name="thumb">
+            <input type="text" class="form-control" id="thumb" name="thumb" value="{{old('thumb')}}">
         </div>
 
         <div class="mb-3">
             <label for="price" class="form-label">Price</label>
-            <input type="text" class="form-control" id="price" name="price">
+            <input type="text" class="form-control" id="price" name="price" value="{{old('price')}}">
         </div>
 
         <div class="mb-3">
             <label for="series" class="form-label">Series</label>
-            <input type="text" class="form-control" id="series" name="series">
+            <input type="text" class="form-control" id="series" name="series" value="{{old('series')}}">
         </div>
 
         <div class="mb-3">
             <label for="sale_date" class="form-label">Sale Date</label>
-            <input type="date" class="form-control" id="sake_date" name="sale_date">
+            <input type="date" class="form-control" id="sake_date" name="sale_date" value="{{old('sale_date')}}">
         </div>
 
         <div class="mb-3">
             <label for="type" class="form-label">Type</label>
             <select class="form-select" id="type" name="type">
-                <option value="comic book">Comic Book</option>
-                <option value="graphic novel">Graphic Novel</option>
+                <option {{(old('type') == 'comic book')? 'selected':''}} value="comic book">Comic Book</option>
+                <option {{(old('type') == 'graphic novel')? 'selected':''}} value="graphic novel">Graphic Novel</option>
             </select>
         </div>
 
